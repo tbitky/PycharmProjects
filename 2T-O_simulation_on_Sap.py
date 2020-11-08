0900512582ef38b41064c0860490815d8f20cfeb
tbitky <tbitky@gmail.com>
2020-11-08 20:01:14 +0900

    mini fixed

--- 2T-O_simulation_on_Sap.py
+++ 2T-O_simulation_on_Sap.py
@@ -3,6 +3,9 @@ from xrayutilities.materials import elements as el
 from matplotlib.pylab import *
 from xrayutilities.materials.material import (Crystal, HexagonalElasticTensor)
 from xrayutilities.materials.spacegrouplattice import SGLattice
+import os
+import tkinter
+from tkinter import filedialog
 
 GaN = Crystal("GaN",
               SGLattice(186, 3.189, 5.186, atoms=[el.Ga, el.N],
@@ -12,12 +15,12 @@ GaN = Crystal("GaN",
 AlN = Crystal('AlN',
               SGLattice(186, 3.1130, 4.9816, atoms=[el.Al, el.N],
                         pos=[('2b', 0), ('2b', 3 / 8.)]),
-              xu.materials.HexagonalElasticTensor(390.e9, 145.e9, 106.e9, 398.e9, 105.e9),
+              xu.materials.HexagonalElasticTensor(410.e9, 149.e9, 99.e9, 389.e9, 125.e9),
               thetaDebye=1150)
 InN = Crystal('AlN',
               SGLattice(186, 3.5380, 5.7020, atoms=[el.In, el.N],
                         pos=[('2b', 0), ('2b', 3 / 8.)]),
-              xu.materials.HexagonalElasticTensor(390.e9, 145.e9, 106.e9, 398.e9, 105.e9),
+              xu.materials.HexagonalElasticTensor(190.e9, 104.e9, 121.e9, 182.e9, 10.e9),
               thetaDebye=660)
 AlGaN = xu.materials.material.Alloy(GaN, AlN, x=0.25)
 
@@ -28,7 +31,7 @@ sub = xu.simpack.Layer(xu.materials.Al2O3, 6300)
 lay1 = xu.simpack.Layer(GaN, 2300)
 lay2 = xu.simpack.Layer(AlN, 1, relaxation=0)
 lay3 = xu.simpack.Layer(AlGaN, 25, relaxation=0)
-epi = xu.simpack.LayerStack('list', sub + lay1 + lay2 + lay3)
+epi = xu.simpack.LayerStack('list',  lay1 + lay2 + lay3)
 
 thetaMono = arcsin(wavelength / (2 * xu.materials.Ge.planeDistance(2, 2, 0)))
 Cmono = cos(2 * thetaMono)
@@ -40,7 +43,7 @@ fitmdyn.set_param_hint('GaN_thickness', vary=True)
 fitmdyn.set_param_hint('GaN_a', vary=True)
 fitmdyn.set_param_hint('GaN_c', vary=True)
 fitmdyn.set_param_hint('AlN_thickness', vary=True)
-fitmdyn.set_param_hint('AlN_a', vary=True)
+fitmdyn.set_param_hint('AlN_a', vary=True,)
 fitmdyn.set_param_hint('AlN_c', vary=True)
 fitmdyn.set_param_hint('GaN_0_75_AlN_0_25__thickness', vary=True)
 fitmdyn.set_param_hint('GaN_0_75_AlN_0_25__a', vary=True)
@@ -52,14 +55,24 @@ params = fitmdyn.make_params()
 
 # plot experimental data
 f = figure(figsize=(7, 5))
-d = xu.io.RASFile('inas_layer_radial_002_004.ras.bz2',
-                  path=r'F:\Users\ore\Downloads')
+
+root = tkinter.Tk()
+root.withdraw()
+fTyp = [("xrdファイル", "*.xrdml")]
+iDir = os.path.abspath(os.path.dirname(r"\\Sirius\carbon-nas\SR4000\003_XRD"))
+filepath = filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
+filename = os.path.basename(filepath)
+dirname = os.path.dirname(filepath)
+d = xu.io.panalytical_xml.XRDMLFile(filename,path=dirname)
 scan = d.scans[-1]
-tt = scan.data[scan.scan_axis] - offset
-semilogy(tt, scan.data['int'], 'o-', ms=3, label='data')
+tt = scan.scanmot - offset
+semilogy(tt, scan.int, 'o-', ms=3, label='data')
 
 # perform fit and plot the result
-fitmdyn.lmodel.set_hkl((0, 0, 4))
-ai = (d.scans[-1].data[d.scan.scan_axis] - offset) / 2
-fitr = fitmdyn.fit(d.scans[-1].data['int'], params, ai)
+fitmdyn.lmodel.set_hkl((0, 0, 2))
+ai = (d.scans[-1].scanmot - offset) / 2
+fitr = fitmdyn.fit(d.scans[-1].int, params, ai)
 print(fitr.fit_report())  # for older lmfit use: lmfit.report_fit(fitr)
+
+
+plt.show()
